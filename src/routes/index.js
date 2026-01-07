ae0dfa841f8dc7ff05ad0dc8d50936071d7d58fe
dinhchinh318 <chinh3182005@gmail.com>
2026-01-07 22:24:39 +0700

    fix: resolve conflict

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // routes/index.js - UPDATED VERSION
 const express = require('express');
 const rateLimit = require('express-rate-limit');
@@ -128,37 +127,6 @@ const initRoute = (app) => {
       suggestion: 'Xem danh sách đầy đủ tại /v1/api'
     });
   });
-=======
-const authRoute = require("../routes/auth.routes");
-const walletRoute = require("../routes/wallet.routes");
-const categoryRoute = require("../routes/category.routes");
-const transactionRoute = require("../routes/transaction.routes");
-const reportRoute = require("../routes/report.routes");
-const analyticsRoute = require("../routes/analytics.routes");
-const budgetRoute = require("../routes/budget.routes");
-const recurringBillRoute = require("../routes/recurringBill.routes");
-const savingGoalRoute = require("../routes/savingGoal.routes");
-const settingRoute = require("../routes/setting.routes");
-const profileRoute = require("../routes/profile.routes");
-const notificationRoute = require("../routes/notification.routes");
-const uploadRoutes = require("./upload.routes");
-
-const initRoute = (app) => {
-    app.use("/v1/api/auth", authRoute);
-    app.use("/v1/api/wallet", walletRoute);
-    app.use("/v1/api/category", categoryRoute);
-    app.use("/v1/api/transaction", transactionRoute);
-    app.use("/v1/api/budget", budgetRoute);
-    app.use("/v1/api/recurring-bill", recurringBillRoute);
-    app.use("/v1/api/saving-goal", savingGoalRoute);
-    app.use("/v1/api/report", reportRoute);
-    app.use("/v1/api/analytics", analyticsRoute);
-    app.use("/v1/api/setting", settingRoute);
-    app.use("/v1/api/profile", profileRoute);
-    app.use("/v1/api/notification", notificationRoute);
-    app.use("/v1/api/upload", uploadRoutes);
-
->>>>>>> 9920827 (fix: vercel)
 }
 
 module.exports = initRoute;

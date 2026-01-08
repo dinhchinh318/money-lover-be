3735c43f45376c785ea8daf61849041608eb3767
dinhchinh318 <chinh3182005@gmail.com>
2026-01-08 21:20:54 +0700

    fix: upload avatar

--- src/models/profile.js
+++ src/models/profile.js
@@ -14,7 +14,16 @@ const profileSchema = new mongoose.Schema(
     // Thông tin hiển thị (User đã có name/avatar/description, nhưng profile giúp mở rộng)
     displayName: { type: String, trim: true, default: "Người Dùng" },
     bio: { type: String, trim: true, default: "" },
-    avatarUrl: { type: String, default: "https://res.cloudinary.com/dijy8yams/image/upload/v1742894461/avatars/lgitn3wbciwcm515y0cb.jpg" },
+    avatarUrl: {
+      type: String,
+      trim: true,
+      default: "https://res.cloudinary.com/dijy8yams/image/upload/v1742894461/avatars/lgitn3wbciwcm515y0cb.jpg",
+      set: (v) => {
+        if (typeof v !== "string") return v;
+        const t = v.trim();
+        return t === "" ? undefined : t; // ✅ để default áp dụng
+      },
+    },
 
     phone: { type: String, trim: true, default: "" },
     address: { type: String, trim: true, default: "" },
